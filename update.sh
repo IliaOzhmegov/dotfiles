b05e57c6dd3ddf9fb73410d8539d6ecdd8d90db1
ilia.ozhmegov <elijahozhmegov@gmail.com>
2022-08-29 21:21:30 +0200

    update zathurarc

--- update.sh
+++ update.sh
@@ -3,3 +3,6 @@
 THISPATH=`pwd`
 ln -s $THISPATH/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s $THISPATH/ideavimrc  ~/ideavimrc
+
+mkdir -p ~/.config/zathura
+ln -s $THISPATH/zathurarc  ~/.config/zathura/zathurarc
